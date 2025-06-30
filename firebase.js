06ca2966f0f6b202754c3290de7dbac72ab169f0
Nikheel0099 <nikheelrathod9900@gmail.com>
2025-06-30 19:59:00 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,13 @@
+// Firebase config (already enabled for GEA site)
+const firebaseConfig = {
+  apiKey: "AIzaSyAmGSYe-iOkba8tlo5KgIWQwt1WDoyYeWa-n0",
+  authDomain: "gujarat-engineering-academy.firebaseapp.com",
+  projectId: "gujarat-engineering-academy",
+  storageBucket: "gujarat-engineering-academy.appspot.com",
+  messagingSenderId: "36947720573",
+  appId: "1:36947720573:web:0c4f521895cb7299b7b414",
+  measurementId: "G-1RJZHCS0H9"
+};
+
+// Firebase Initialization
+firebase.initializeApp(firebaseConfig);
