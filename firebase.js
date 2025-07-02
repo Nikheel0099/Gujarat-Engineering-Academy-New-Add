ac87e92c18bbf47ea1be0c6fc78de44e4fe35f40
Nikheel0099 <nikheelrathod9900@gmail.com>
2025-07-02 07:28:10 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,11 @@
 // firebase.js
 
-// Firebase SDK ના જરૂરી ફંક્શન ઇમ્પોર્ટ કરો
+// Firebase ના modules import કરો
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-analytics.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
-// તમારું Firebase પ્રોજેક્ટ config
+// તમારું Firebase config (Gujarat Engineering Academy project)
 const firebaseConfig = {
   apiKey: "AIzaSyAmGSYe-IOkba8to5KglWQwtWDoYeWa-n0",
   authDomain: "gujarat-engineering-academy.firebaseapp.com",
@@ -16,12 +16,12 @@ const firebaseConfig = {
   measurementId: "G-1RJHZHCS0H"
 };
 
-// Firebase App ને Initialize કરો
+// Firebase App ને initialize કરો
 const app = initializeApp(firebaseConfig);
 
-// Analytics અને Authentication initial કરો
+// Firebase Services setup
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
 
-// Export auth object for use in login/register
+// auth export કરો જેથી register/login.js માં વાપરી શકાય
 export { auth };
