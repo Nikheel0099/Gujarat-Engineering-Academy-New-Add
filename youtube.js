fc35c4069de3b0652a7e8c72bb770bf47360dac5
Nikheel0099 <nikheelrathod9900@gmail.com>
2025-07-04 14:18:20 +0530

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -1,39 +1,32 @@
 // youtube.js
-const apiKey = "AIzaSyC05S7rCzyWTR8Xn5WBw-SrflmF-kclO5Y";
+const apiKey = "AIzaSyC05S7rCzyWTR8Xn5WBw-SrflO5Y"; // YouTube API Key
 const channelId = "UCJ8RxJgqv-UeXZbJbQaQ_TA";
-const videoContainer = document.getElementById("youtubeVideos");
-
-let nextPageToken = "";
+const container = document.getElementById("youtubeVideos");
 let allVideos = [];
+let nextToken = "";
 
 function fetchVideos(pageToken = "") {
   const url = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&channelId=${channelId}&part=snippet,id&order=date&maxResults=50&pageToken=${pageToken}`;
-
-  fetch(url)
-    .then(res => res.json())
-    .then(data => {
-      allVideos = allVideos.concat(data.items.filter(item => item.id.kind === "youtube#video"));
-
-      if (data.nextPageToken) {
-        fetchVideos(data.nextPageToken);
-      } else {
-        renderVideos(allVideos);
-      }
-    })
-    .catch(err => {
-      console.error("Error loading videos: ", err);
-      videoContainer.innerHTML = "<p>Failed to load videos.</p>";
-    });
+  fetch(url).then(r => r.json()).then(data => {
+    allVideos = allVideos.concat(data.items.filter(i => i.id.kind === "youtube#video"));
+    if (data.nextPageToken) fetchVideos(data.nextPageToken);
+    else showVideos();
+  }).catch(e => {
+    console.error(e);
+    container.innerHTML = "<p>Failed to load videos.</p>";
+  });
 }
 
-function renderVideos(videos) {
-  videoContainer.innerHTML = "";
-  videos.forEach(item => {
-    videoContainer.innerHTML += `
-      <div class="video-card">
-        <iframe src="https://www.youtube.com/embed/${item.id.videoId}" allowfullscreen></iframe>
-        <p>${item.snippet.title}</p>
-      </div>`;
+function showVideos() {
+  container.innerHTML = "";
+  allVideos.forEach(v => {
+    const div = document.createElement("div");
+    div.className = "video-card";
+    div.innerHTML = `
+      <iframe src="https://www.youtube.com/embed/${v.id.videoId}" allowfullscreen></iframe>
+      <p>${v.snippet.title}</p>
+    `;
+    container.append(div);
   });
 }
 
